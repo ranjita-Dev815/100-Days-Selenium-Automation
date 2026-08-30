63e94464d83defec329f87c23bd8891151870625
ranjita-Dev815 <prustyranjita950@gmail.com>
2026-08-31 00:29:02 +0530

    Add files via upload

---
+++ GoogleSearchTest_Day1.java
@@ -0,0 +1,67 @@
+package Seleniumdailypratics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class GoogleSearchTest_Day1 {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setup() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void GoogleSearchTest_Day1() {
+
+        WebElement searchBox =
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+
+        searchBox.sendKeys("selenium");
+
+        searchBox.submit();
+
+        wait.until(ExpectedConditions.titleContains("selenium"));
+
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
+
+        System.out.println("Page title: " + title);
+        System.out.println("Current URL: " + url);
+
+        Assert.assertTrue(url.contains("google"), "URL validation failed...");
+
+        System.out.println("Google search test passed....");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
+
+
